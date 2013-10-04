61c884014bf721a942835d34261aa83a38410192
Cekurte Sistemas <sistemas@cekurte.com>
2013-10-04 11:10:33 -0300

    Adicionado o modelo de banco de dados que iremos desenvolver ao decorrer do mini-curso e também os slides de referência.

---
+++ docs/database/PHP do Jeito Certo.mm
@@ -0,0 +1,133 @@
+
+<map version="0.9.0">
+    <node TEXT="Mind Map">
+        <node ID="D5A82D80-6803-46C2-A380-0B1FD3F767DF" TEXT="Introdução ao Desenvolvimento Web" FOLDED="false" COLOR="#000000">
+            <font NAME="LucidaGrande" SIZE="13"></font>
+            <edge COLOR="#7F7F7F"></edge>
+            <node ID="42E67833-D4CF-45C2-A63E-E1DCEEA00D67" TEXT="HTTP" POSITION="left" FOLDED="false" COLOR="#000000">
+                <font NAME="LucidaGrande" SIZE="12"></font>
+                <edge COLOR="#66FFCC"></edge>
+            </node>
+            <node ID="89982342-21E7-4565-BA96-7B1165E2F192" TEXT="Apache" POSITION="left" FOLDED="false" COLOR="#000000">
+                <font NAME="LucidaGrande" SIZE="12"></font>
+                <edge COLOR="#CCFF66"></edge>
+            </node>
+            <node ID="072C1CE4-769E-4EB5-B0D5-548BBE665A99" TEXT="1:00 - CSS e o CSS3" POSITION="right" FOLDED="false" COLOR="#000000">
+                <font NAME="LucidaGrande" SIZE="12"></font>
+                <edge COLOR="#CC66FF"></edge>
+                <node ID="8DE370A8-E45B-444A-BFE4-324E5EC85B66" TEXT="Twitter Bootstrap" FOLDED="false" COLOR="#000000">
+                    <font NAME="LucidaGrande" SIZE="12"></font>
+                    <edge COLOR="#CC66FF"></edge>
+                </node>
+            </node>
+            <node ID="9772CDEE-FEFE-4FC3-B193-A7D12F1B7E21" TEXT="0:50 - HTML e o HTML5 (conceitos básicos)" POSITION="left" FOLDED="false" COLOR="#000000">
+                <font NAME="LucidaGrande" SIZE="12"></font>
+                <edge COLOR="#66CCFF"></edge>
+            </node>
+            <node ID="723060CB-EDC8-410C-BE44-A210E0F2790C" TEXT="1:30 - Javascript e o jQuery" POSITION="right" FOLDED="false" COLOR="#000000">
+                <font NAME="LucidaGrande" SIZE="12"></font>
+                <edge COLOR="#FFE57F"></edge>
+                <node ID="2FA9D9DF-D73B-4A19-A493-AFE1AD80E19E" TEXT="Validação de Formulários" FOLDED="false" COLOR="#000000">
+                    <font NAME="LucidaGrande" SIZE="12"></font>
+                    <edge COLOR="#FFE57F"></edge>
+                </node>
+            </node>
+        </node>
+        <node ID="ACA6F0BA-D8B5-4446-B96A-7667F7D107C0" TEXT="PHP do Jeito Certo" FOLDED="false" COLOR="#000000">
+            <font NAME="LucidaGrande" SIZE="13"></font>
+            <edge COLOR="#7F7F7F"></edge>
+            <node ID="2D67F093-7D85-4D59-B96F-784F4F0F63FB" TEXT="0:45 - Git" POSITION="left" FOLDED="false" COLOR="#000000">
+                <font NAME="LucidaGrande" SIZE="12"></font>
+                <edge COLOR="#E5B24C"></edge>
+            </node>
+            <node ID="87FD0D6B-33E3-45C8-9BD0-8B8C6373EA96" TEXT="0:45 - Gerenciamento de dependência" POSITION="left" FOLDED="false" COLOR="#000000">
+                <font NAME="LucidaGrande" SIZE="12"></font>
+                <edge COLOR="#FFE57F"></edge>
+                <node ID="C373AF2D-EFA5-4F01-AE35-97494B256D8E" TEXT="Composer" FOLDED="false" COLOR="#000000">
+                    <font NAME="LucidaGrande" SIZE="12"></font>
+                    <edge COLOR="#FFE57F"></edge>
+                </node>
+                <node ID="124CC090-5A54-415E-A19D-BF149CAEC1AD" TEXT="Bower" FOLDED="false" COLOR="#000000">
+                    <font NAME="LucidaGrande" SIZE="12"></font>
+                    <edge COLOR="#FFE57F"></edge>
+                </node>
+            </node>
+            <node ID="E7F0B7CB-CED8-46C7-B1AE-C88881704608" TEXT="Web dinâmica" POSITION="left" FOLDED="false" COLOR="#000000">
+                <font NAME="LucidaGrande" SIZE="12"></font>
+                <edge COLOR="#B2E54C"></edge>
+                <node ID="458E5641-028B-49C0-B533-B601A3D1D716" TEXT="PHP e o Sobrinho" FOLDED="false" COLOR="#000000">
+                    <font NAME="LucidaGrande" SIZE="12"></font>
+                    <edge COLOR="#B2E54C"></edge>
+                    <node ID="16949774-DD04-4AF6-844B-F6B4A93CE63A" TEXT="Exemplos" FOLDED="false" COLOR="#000000">
+                        <font NAME="LucidaGrande" SIZE="12"></font>
+                        <edge COLOR="#B2E54C"></edge>
+                    </node>
+                </node>
+                <node ID="4AC8A9F9-7388-49B6-A0BB-63C72146E2B4" TEXT="Design Pattern MVC" FOLDED="false" COLOR="#000000">
+                    <font NAME="LucidaGrande" SIZE="12"></font>
+                    <edge COLOR="#B2E54C"></edge>
+                    <node ID="26581D76-E7E9-4785-8396-4FF61D75B5F4" TEXT="Frameworks" FOLDED="false" COLOR="#000000">
+                        <font NAME="LucidaGrande" SIZE="12"></font>
+                        <edge COLOR="#B2E54C"></edge>
+                    </node>
+                    <node ID="3184C7FB-57C4-4F77-B0CF-146C39EF629D" TEXT="6:00 - PHP do Jeito" FOLDED="false" COLOR="#000000">
+                        <font NAME="LucidaGrande" SIZE="12"></font>
+                        <edge COLOR="#B2E54C"></edge>
+                        <node ID="77476CC8-7BCA-43F0-8787-02C09A87EC43" TEXT="Controller" FOLDED="false" COLOR="#000000">
+                            <font NAME="LucidaGrande" SIZE="12"></font>
+                            <edge COLOR="#B2E54C"></edge>
+                        </node>
+                        <node ID="F7F83E18-D153-4128-8C33-ABD39657CC9F" TEXT="Model" FOLDED="false" COLOR="#000000">
+                            <font NAME="LucidaGrande" SIZE="12"></font>
+                            <edge COLOR="#B2E54C"></edge>
+                        </node>
+                        <node ID="7D2995D4-340B-4A6F-A51D-D7D92B92F893" TEXT="View" FOLDED="false" COLOR="#000000">
+                            <font NAME="LucidaGrande" SIZE="12"></font>
+                            <edge COLOR="#B2E54C"></edge>
+                        </node>
+                    </node>
+                </node>
+                <node ID="3EF57FC2-F8E1-46BC-BD9C-01A23F91EEA9" TEXT="Prática" FOLDED="false" COLOR="#000000">
+                    <font NAME="LucidaGrande" SIZE="12"></font>
+                    <edge COLOR="#B2E54C"></edge>
+                    <node ID="46D7D13F-5360-48B2-905B-CB069331DB5F" TEXT="Blog" FOLDED="false" COLOR="#000000">
+                        <font NAME="LucidaGrande" SIZE="12"></font>
+                        <edge COLOR="#B2E54C"></edge>
+                        <node ID="BC0E5524-A460-4969-AD7E-4245A5BE8F53" TEXT="Admin" FOLDED="false" COLOR="#000000">
+                            <font NAME="LucidaGrande" SIZE="12"></font>
+                            <edge COLOR="#B2E54C"></edge>
+                            <node ID="B0659EB3-05BD-4385-B492-E16F51B0C59D" TEXT="Post" FOLDED="false" COLOR="#000000">
+                                <font NAME="LucidaGrande" SIZE="12"></font>
+                                <edge COLOR="#B2E54C"></edge>
+                            </node>
+                            <node ID="CB792CFD-29E3-4BFB-ACCD-596F64BF8AED" TEXT="Categoria" FOLDED="false" COLOR="#000000">
+                                <font NAME="LucidaGrande" SIZE="12"></font>
+                                <edge COLOR="#B2E54C"></edge>
+                            </node>
+                            <node ID="DCA4BA61-7AB2-4A96-A1B3-3505D11E90FD" TEXT="Autenticação" FOLDED="false" COLOR="#000000">
+                                <font NAME="LucidaGrande" SIZE="12"></font>
+                                <edge COLOR="#B2E54C"></edge>
+                            </node>
+                        </node>
+                        <node ID="118AEE87-BDD8-4166-8D5E-1EBAB2D4AF75" TEXT="Front-End" FOLDED="false" COLOR="#000000">
+                            <font NAME="LucidaGrande" SIZE="12"></font>
+                            <edge COLOR="#B2E54C"></edge>
+                            <node ID="06AF99A7-EA05-407B-AA7E-AF6DC87151E3" TEXT="Mostrar os Posts" FOLDED="false" COLOR="#000000">
+                                <font NAME="LucidaGrande" SIZE="12"></font>
+                                <edge COLOR="#B2E54C"></edge>
+                            </node>
+                            <node ID="76121BE7-B91A-4EBC-B249-01460F6D2A2E" TEXT="Mostar as Categorias" FOLDED="false" COLOR="#000000">
+                                <font NAME="LucidaGrande" SIZE="12"></font>
+                                <edge COLOR="#B2E54C"></edge>
+                            </node>
+                            <node ID="7BFE969B-14BB-4A01-AD56-CCBFA1499500" TEXT="Mostrar os Posts de uma Categoria" FOLDED="false" COLOR="#000000">
+                                <font NAME="LucidaGrande" SIZE="12"></font>
+                                <edge COLOR="#B2E54C"></edge>
+                            </node>
+                        </node>
+                    </node>
+                </node>
+            </node>
+        </node>
+    </node>
+</map>
---
+++ docs/database/php_do_jeito_certo.sql
@@ -0,0 +1,31 @@
+CREATE SCHEMA IF NOT EXISTS `php_do_jeito_certo` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci ;
+USE `php_do_jeito_certo` ;
+
+-- -----------------------------------------------------
+-- Table `php_do_jeito_certo`.`sexo`
+-- -----------------------------------------------------
+CREATE  TABLE IF NOT EXISTS `php_do_jeito_certo`.`sexo` (
+  `sigla` CHAR(1) NOT NULL ,
+  `nome` VARCHAR(255) NOT NULL ,
+  PRIMARY KEY (`sigla`) ,
+  UNIQUE INDEX `sigla_UNIQUE` (`sigla` ASC) )
+ENGINE = InnoDB;
+
+-- -----------------------------------------------------
+-- Table `php_do_jeito_certo`.`agenda`
+-- -----------------------------------------------------
+CREATE  TABLE IF NOT EXISTS `php_do_jeito_certo`.`agenda` (
+  `id` INT UNSIGNED NOT NULL AUTO_INCREMENT ,
+  `nome` VARCHAR(255) NOT NULL ,
+  `email` VARCHAR(150) NULL ,
+  `telefone` CHAR(14) NOT NULL ,
+  `sexo_sigla` CHAR(1) NOT NULL ,
+  PRIMARY KEY (`id`) ,
+  UNIQUE INDEX `id_UNIQUE` (`id` ASC) ,
+  INDEX `fk_agenda_sexo_idx` (`sexo_sigla` ASC) ,
+  CONSTRAINT `fk_agenda_sexo`
+    FOREIGN KEY (`sexo_sigla` )
+    REFERENCES `php_do_jeito_certo`.`sexo` (`sigla` )
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
